2a4d5a5cfd23e086820d72fc5185d28e0a16266b
JUNE <jh010303@naver.com>
2025-05-04 22:28:24 +0900

    :sparkles: feat: Parent-Child 중간 테이블 구현

---
+++ src/main/java/final_project/momeasy/domain/parent/entity/ParentChild.java
@@ -0,0 +1,28 @@
+package final_project.momeasy.domain.parent.entity;
+
+import final_project.momeasy.common.enums.Relation;
+import final_project.momeasy.domain.child.entity.Child;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ParentChild {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Relation relation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id", nullable = false)
+    private Child child;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", nullable = false)
+    private Parent parent;
+}
