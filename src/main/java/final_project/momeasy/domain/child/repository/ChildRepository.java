3ad6d4b6b6043c529519c19300ae7e6934bf1f68
JUNE <jh010303@naver.com>
2025-05-04 22:54:03 +0900

    :sparkles: child 리포지터리 구현

---
+++ src/main/java/final_project/momeasy/domain/child/repository/ChildRepository.java
@@ -0,0 +1,16 @@
+package final_project.momeasy.domain.child.repository;
+
+import final_project.momeasy.domain.child.entity.Child;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChildRepository extends JpaRepository<Child, Long> {
+    Optional<Child> findById(Long id);
+
+    @Query("SELECT c FROM Child c JOIN FETCH ParentChild pc ON c.id = pc.child.id WHERE pc.parent.id = :parentId")
+    List<Child> findByParentId(@Param("parentId") Long parentId);
+}
