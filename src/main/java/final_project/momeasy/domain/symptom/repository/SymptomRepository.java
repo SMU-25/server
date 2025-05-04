40cf2cb2f31007d928243541a5b2adce60e3225b
JUNE <jh010303@naver.com>
2025-05-04 22:57:10 +0900

    :sparkles: Symptom 리포지터리 구현

---
+++ src/main/java/final_project/momeasy/domain/symptom/repository/SymptomRepository.java
@@ -0,0 +1,14 @@
+package final_project.momeasy.domain.symptom.repository;
+
+import final_project.momeasy.domain.illness.entity.Illness;
+import final_project.momeasy.domain.symptom.entity.Symptom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SymptomRepository extends JpaRepository<Symptom, Long> {
+    @Query("select s from Symptom s join fetch FeverReportSymptom fs on s.id = fs.symptom.id where fs.feverreport.id =:feverreportId")
+    List<Symptom> findByFeverReportId(@Param("feverreportId") Long FeverReportId);
+}
