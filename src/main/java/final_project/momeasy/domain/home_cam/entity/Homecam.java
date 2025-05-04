b845043085d463367d42a6a2e42fbd3f7fcff23d
JUNE <jh010303@naver.com>
2025-05-04 22:27:14 +0900

    :sparkles: feat: Homecam 엔티티 구현

---
+++ src/main/java/final_project/momeasy/domain/home_cam/entity/Homecam.java
@@ -0,0 +1,39 @@
+package final_project.momeasy.domain.home_cam.entity;
+
+import final_project.momeasy.domain.child.entity.Child;
+import final_project.momeasy.domain.parent.entity.Parent;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Homecam {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String serial_num;
+
+    @Column(nullable = false)
+    private String place;
+
+    @Column(nullable = false)
+    private String date;
+
+    private String video_url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id",nullable = false)
+    private Parent parent;
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+        parent.getHomecams().add(this);
+    }
+}
