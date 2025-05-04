d82c8100d3f5eab88d283204ca87d4c4fd524863
JUNE <jh010303@naver.com>
2025-05-04 22:56:26 +0900

    :sparkles: Illness 리포지터리 구현

---
+++ src/main/java/final_project/momeasy/domain/illness/repository/IllnessRepository.java
@@ -0,0 +1,15 @@
+package final_project.momeasy.domain.illness.repository;
+
+
+import final_project.momeasy.domain.illness.entity.Illness;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IllnessRepository extends JpaRepository<Illness, Long> {
+
+    @Query("select i from Illness i join fetch ChildIllness ci on i.id = ci.illness.id where ci.child.id = :childId")
+    List<Illness> findByChildId(@Param("childId") Long childId);
+}
