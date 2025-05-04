f94b0bfad67ad442684ec0bd58a7e0aec390be02
JUNE <jh010303@naver.com>
2025-05-04 22:25:17 +0900

    :sparkles: feat: FeverRecord 엔티티 구현

---
+++ src/main/java/final_project/momeasy/domain/fever_record/entity/FeverRecord.java
@@ -0,0 +1,28 @@
+package final_project.momeasy.domain.fever_record.entity;
+
+import final_project.momeasy.domain.child.entity.Child;
+import final_project.momeasy.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class FeverRecord extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private float fever;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id",nullable = false)
+    private Child child;
+
+    public void setChild(Child child) {
+        this.child = child;
+        child.getFeverRecords().add(this);
+    }
+}
