b1fc85de8a2dd366d9b520920191e781af95c420
JUNE <jh010303@naver.com>
2025-05-04 22:24:16 +0900

    :sparkles: feat: Calendar 엔티티 구현

---
+++ src/main/java/final_project/momeasy/domain/calendar/entity/Calendar.java
@@ -0,0 +1,35 @@
+package final_project.momeasy.domain.calendar.entity;
+
+import final_project.momeasy.domain.parent.entity.Parent;
+import final_project.momeasy.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Calendar extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate recordDate;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id",nullable = false)
+    private Parent parent;
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+        parent.getCalendars().add(this);
+    }
+}
