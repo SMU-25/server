f878e6e19a988dcd7a10b1d6ef09c6eee44d7547
JUNE <jh010303@naver.com>
2025-05-14 19:32:19 +0900

    :sparkles: feat: 홈캠 요청 DTO 구현

---
+++ src/main/java/final_project/momeasy/domain/home_cam/dto/HomecamRequestDTO.java
@@ -0,0 +1,28 @@
+package final_project.momeasy.domain.home_cam.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+public class HomecamRequestDTO {
+    @Getter
+    @Builder
+    public static class HomecamRegisterDTO {
+        @NotNull(message = "기기 번호는 필수 입력 값입니다.")
+        @JsonProperty("serial_num")
+        private String serial_num;
+
+        @NotBlank(message = "기기 이름은 필수 입력 값입니다.")
+        @JsonProperty("name")
+        private String name;
+
+        @NotBlank(message = "설치 장소는 필수 입력 값입니다.")
+        @JsonProperty("place")
+        private String place;
+
+        @NotBlank(message = "설치 날짜는 필수 입력 값입니다.")
+        @JsonProperty("date")
+        private String date;
+    }
+}
