57a478955c6cebd7622bbb07b5c4028514410a3a
JUNE <jh010303@naver.com>
2025-05-14 17:18:45 +0900

    :sparkles: feat: 홈캠 예외처리 구현

---
+++ src/main/java/final_project/momeasy/domain/home_cam/exception/HomecamErrorCode.java
@@ -0,0 +1,22 @@
+package final_project.momeasy.domain.home_cam.exception;
+
+import final_project.momeasy.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum HomecamErrorCode implements BaseErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "HOMECAM404", "홈캠을 찾을 수 없습니다."),
+    ALREADY_ADD(HttpStatus.CONFLICT, "HOMECAM409", "이미 등록된 홈캠입니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "HOMECAM404", "홈캠에 접근할 수 있는 권한이 없습니다."),
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "HOMECAM500", "홈캠 처리 중 서버 오류가 발생했습니다"),
+    UPDATE_FAILED(HttpStatus.BAD_REQUEST, "HOMECAM4002", "홈캠 정보를 수정할 수 없습니다"),
+    DELETE_FAILED(HttpStatus.BAD_REQUEST, "HOMECAM4003", "홈캠 정보를 삭제할 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
