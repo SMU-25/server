24cca336972cfd59708cc1b34f9f8dae42c4c4f0
JUNE <jh010303@naver.com>
2025-05-16 00:47:02 +0900

    :hammer: refactor: 인터페이스 사용

--- src/main/java/final_project/momeasy/domain/home_cam/controller/HomecamController.java
+++ src/main/java/final_project/momeasy/domain/home_cam/controller/HomecamController.java
@@ -2,7 +2,9 @@ package final_project.momeasy.domain.home_cam.controller;
 
 import final_project.momeasy.domain.home_cam.dto.HomecamRequestDTO;
 import final_project.momeasy.domain.home_cam.dto.HomecamResponseDTO;
+import final_project.momeasy.domain.home_cam.service.HomecamQueryService;
 import final_project.momeasy.domain.home_cam.service.HomecamQueryServiceImpl;
+import final_project.momeasy.domain.home_cam.service.HomecamService;
 import final_project.momeasy.domain.home_cam.service.HomecamServiceImpl;
 import final_project.momeasy.domain.parent.entity.Parent;
 import final_project.momeasy.global.apiPayload.CustomResponse;
@@ -17,14 +19,14 @@ import java.util.List;
 @RequestMapping("/homecam")
 public class HomecamController {
     // security 완성되면 @AuthenticatedMember 추가하기
-    private final HomecamQueryServiceImpl homecamQueryServiceImpl;
-    private final HomecamServiceImpl homecamServiceImpl;
+    private final HomecamQueryService homecamQueryService;
+    private final HomecamService homecamService;
 
     @GetMapping("/{homecamId}")
     public CustomResponse<HomecamResponseDTO.HomecamDTO> getHomecam(
             @AuthenticationPrincipal Parent parent,
             @PathVariable("homecamId") long homecamId) {
-        HomecamResponseDTO.HomecamDTO homecam = homecamQueryServiceImpl.getHomecamById(homecamId,parent);
+        HomecamResponseDTO.HomecamDTO homecam = homecamQueryService.getHomecamById(homecamId,parent);
         return CustomResponse.onSuccess(homecam);
     }
 
@@ -32,19 +34,19 @@ public class HomecamController {
     public CustomResponse<HomecamResponseDTO.HomecamVideoDTO> getHomecamVideo(
             @AuthenticationPrincipal Parent parent,
             @PathVariable("homecamId") long homecamId) {
-        HomecamResponseDTO.HomecamVideoDTO homecam = homecamQueryServiceImpl.getHomecamVideoById(homecamId, parent);
+        HomecamResponseDTO.HomecamVideoDTO homecam = homecamQueryService.getHomecamVideoById(homecamId, parent);
         return CustomResponse.onSuccess(homecam);
     }
 
     @GetMapping("/list")
     public CustomResponse<List<HomecamResponseDTO.HomecamDTO>> getHomecams(@AuthenticationPrincipal Parent parent) {
-        List<HomecamResponseDTO.HomecamDTO> homecamList = homecamQueryServiceImpl.getHomecamListByParent(parent);
+        List<HomecamResponseDTO.HomecamDTO> homecamList = homecamQueryService.getHomecamListByParent(parent);
         return CustomResponse.onSuccess(homecamList);
     }
 
     @DeleteMapping("/{homecamId}")
     public CustomResponse<Void> deleteHomecam(@PathVariable("homecamId") long homecamId, @AuthenticationPrincipal Parent parent) {
-        homecamServiceImpl.deleteHomecam(homecamId,parent);
+        homecamService.deleteHomecam(homecamId,parent);
         return CustomResponse.onSuccess(null);
     }
 
@@ -52,7 +54,7 @@ public class HomecamController {
     public CustomResponse<HomecamResponseDTO.HomecamDTO> createHomecam(
             @AuthenticationPrincipal Parent parent,
             @RequestBody HomecamRequestDTO.HomecamRegisterDTO homecamRegisterDTO) {
-        HomecamResponseDTO.HomecamDTO homecam = homecamServiceImpl.createHomecam(homecamRegisterDTO,parent);
+        HomecamResponseDTO.HomecamDTO homecam = homecamService.createHomecam(homecamRegisterDTO,parent);
         return CustomResponse.onSuccess(homecam);
     }
 }
