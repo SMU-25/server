89322e7d6fdc70a905587858c308849aa822f784
JUNE <jh010303@naver.com>
2025-05-16 12:19:30 +0900

    :sparkles: feat: 홈캠 유무 예외처리 추가

--- src/main/java/final_project/momeasy/domain/fever_record/service/FeverRecordQueryServiceImpl.java
+++ src/main/java/final_project/momeasy/domain/fever_record/service/FeverRecordQueryServiceImpl.java
@@ -1,15 +1,14 @@
 package final_project.momeasy.domain.fever_record.service;
 
-import final_project.momeasy.domain.child.entity.Child;
 import final_project.momeasy.domain.child.repository.ChildRepository;
 import final_project.momeasy.domain.fever_record.converter.FeverRecordConverter;
-import final_project.momeasy.domain.fever_record.dto.FeverRecordRequestDTO;
 import final_project.momeasy.domain.fever_record.dto.FeverRecordResponseDTO;
 import final_project.momeasy.domain.fever_record.entity.FeverRecord;
 import final_project.momeasy.domain.fever_record.exception.FeverRecordErrorCode;
 import final_project.momeasy.domain.fever_record.exception.FeverRecordException;
 import final_project.momeasy.domain.fever_record.repository.FeverRecordRepository;
-import final_project.momeasy.domain.parent.repository.ParentRepository;
+import final_project.momeasy.domain.home_cam.exception.HomecamException;
+import final_project.momeasy.domain.home_cam.repository.HomecamRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
@@ -22,6 +21,7 @@ import java.util.List;
 public class FeverRecordQueryServiceImpl implements FeverRecordQueryService {
     private final FeverRecordRepository feverRecordRepository;
     private final ChildRepository childRepository;
+    private final HomecamRepository homecamRepository;
 
     // 아이에게 홈캠이 있는지 예외 처리 필요
     @Override
@@ -35,6 +35,7 @@ public class FeverRecordQueryServiceImpl implements FeverRecordQueryService {
 
     @Override
     public List<FeverRecordResponseDTO.FeverRecordViewDTO> getFeverRecordPage(Long childId, int page) {
+        homecamRepository.findById(childId).orElseThrow(()->new HomecamException(FeverRecordErrorCode.NOT_FOUND));
         PageRequest pageRequest = PageRequest.of(page, 10);
         Slice<FeverRecord> feverRecords = feverRecordRepository.findAllByChildIdOrderByIdDesc(childId,pageRequest);
         List<FeverRecordResponseDTO.FeverRecordViewDTO> feverRecordsDTO = feverRecords.stream().map(FeverRecordConverter::toFeverRecordResponseDTO).toList();
@@ -43,6 +44,7 @@ public class FeverRecordQueryServiceImpl implements FeverRecordQueryService {
 
     @Override
     public List<FeverRecordResponseDTO.FeverRecordViewDTO> getFeverRecordList(Long childId) {
+        homecamRepository.findById(childId).orElseThrow(()->new HomecamException(FeverRecordErrorCode.NOT_FOUND));
         List<FeverRecord> feverRecords = feverRecordRepository.findAllByChildId(childId);
         List<FeverRecordResponseDTO.FeverRecordViewDTO> feverRecordsDTO = feverRecords.stream().map(FeverRecordConverter::toFeverRecordResponseDTO).toList();
         return feverRecordsDTO;
