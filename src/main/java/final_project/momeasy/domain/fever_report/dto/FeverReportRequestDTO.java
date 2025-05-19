24553eeab7664f2c38d1544caabe76b3272585ec
JUNE <jh010303@naver.com>
2025-05-20 07:24:34 +0900

    :sparkles: feat: 발열 리포트 요청 DTO 구현

---
+++ src/main/java/final_project/momeasy/domain/fever_report/dto/FeverReportRequestDTO.java
@@ -0,0 +1,23 @@
+package final_project.momeasy.domain.fever_report.dto;
+
+import final_project.momeasy.domain.symptom.entity.Symptom;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+public class FeverReportRequestDTO {
+    @Getter
+    @Builder
+    public static class FeverReportCreateDTO {
+        @NotBlank(message = "증상은 필수 입력 값입니다.")
+        private List<Symptom> symptoms;
+
+        private String etc_symptom;
+
+        @NotNull(message = "외출 기록은 필수 입력 값입니다.")
+        private String outing;
+    }
+}
