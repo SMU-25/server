93fc125645f97e205022334521b208e593a40d57
JUNE <jh010303@naver.com>
2025-05-27 20:17:14 +0900

    :sparkles: feat: 발열 리포트 DTO<->Entity Converter 구현

--- src/main/java/final_project/momeasy/domain/fever_report/converter/FeverReportConverter.java
+++ src/main/java/final_project/momeasy/domain/fever_report/converter/FeverReportConverter.java
@@ -1,27 +1,30 @@
 package final_project.momeasy.domain.fever_report.converter;
 
 
+import final_project.momeasy.common.enums.SymptomType;
 import final_project.momeasy.domain.child.entity.Child;
 import final_project.momeasy.domain.fever_report.dto.FeverReportRequestDTO;
 import final_project.momeasy.domain.fever_report.dto.FeverReportResponseDTO;
 import final_project.momeasy.domain.fever_report.entity.FeverReport;
 import final_project.momeasy.domain.fever_report.entity.FeverReportSymptom;
+import final_project.momeasy.domain.symptom.entity.Symptom;
+
+import java.util.List;
 
 public class FeverReportConverter {
-    public static FeverReportResponseDTO.FeverReportViewDTO toFeverReportViewDTO(FeverReport feverReport,String special) {
+    public static FeverReportResponseDTO.FeverReportViewDTO toFeverReportViewDTO(FeverReport feverReport) {
         return FeverReportResponseDTO.FeverReportViewDTO.builder()
                 .outing(feverReport.getOuting())
-                .special(special)
+                .special(feverReport.getSpecial())
                 .etc_symptom(feverReport.getEtc_symptom())
-                .symptoms(feverReport.getFeverReportSymptoms().stream().map(FeverReportSymptom::getSymptom).toList())
+                .symptoms(feverReport.getFeverReportSymptoms().stream().map(FeverReportSymptom -> FeverReportSymptom.getSymptom().getSymptom()).toList())
                 .build();
     }
 
-    public static FeverReport toFeverReport(FeverReportRequestDTO.FeverReportCreateDTO feverReportCreateDTO, Child child, String special) {
+    public static FeverReport toFeverReport(FeverReportRequestDTO.FeverReportCreateDTO feverReportCreateDTO, String special) {
         return FeverReport.builder()
                 .outing(feverReportCreateDTO.getOuting())
                 .special(special)
-                .child(child)
                 .build();
     }
 }
