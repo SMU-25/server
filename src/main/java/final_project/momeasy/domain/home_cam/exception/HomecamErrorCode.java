56d414770d4f50f83d7c7f0856f1228b76a53fb6
jh010303 <jh010303@naver.com>
2025-06-02 23:11:14 +0900

    :sparkles: feat: 홈캠 에러 코드 추가

--- src/main/java/final_project/momeasy/domain/home_cam/exception/HomecamErrorCode.java
+++ src/main/java/final_project/momeasy/domain/home_cam/exception/HomecamErrorCode.java
@@ -13,6 +13,7 @@ public enum HomecamErrorCode implements BaseErrorCode {
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "HOMECAM401", "홈캠에 접근할 수 있는 권한이 없습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "HOMECAM404", "홈캠을 찾을 수 없습니다."),
     ALREADY_ADD(HttpStatus.CONFLICT, "HOMECAM409", "이미 등록된 홈캠입니다."),
+    ALREADY_HAVE(HttpStatus.CONFLICT, "HOMECAM409_2", "이미 홈캠이 있습니다."),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "HOMECAM500", "홈캠 처리 중 서버 오류가 발생했습니다");
 
     private final HttpStatus status;
