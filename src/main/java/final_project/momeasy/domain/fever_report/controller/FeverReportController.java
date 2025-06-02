17a67b8e432c237dfaa75f43618412d6a0e320f3
JUNE <jh010303@naver.com>
2025-06-03 00:58:50 +0900

    :sparkles: feat: 컨트롤러 @AuthParent 사용

--- src/main/java/final_project/momeasy/domain/fever_report/controller/FeverReportController.java
+++ src/main/java/final_project/momeasy/domain/fever_report/controller/FeverReportController.java
@@ -4,12 +4,12 @@ import final_project.momeasy.domain.fever_report.dto.FeverReportRequestDTO;
 import final_project.momeasy.domain.fever_report.dto.FeverReportResponseDTO;
 import final_project.momeasy.domain.fever_report.service.FeverReportQueryService;
 import final_project.momeasy.domain.fever_report.service.FeverReportService;
+import final_project.momeasy.domain.parent.entity.Parent;
 import final_project.momeasy.global.apiPayload.CustomResponse;
-import final_project.momeasy.global.security.CustomUserDetails;
+import final_project.momeasy.global.security.annotation.AuthParent;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,33 +24,42 @@ public class FeverReportController {
 
     @GetMapping("/{childId}/{reportId}")
     @Operation(summary = "발열 리포트 1개 조회 API", description = "발열 리포트를 1개 조회합니다.")
-    public CustomResponse<FeverReportResponseDTO.FeverReportViewDTO> getFeverReport(@AuthenticationPrincipal CustomUserDetails userDetails,
+    public CustomResponse<FeverReportResponseDTO.FeverReportViewDTO> getFeverReport(@AuthParent Parent parent,
         @PathVariable("childId") long childId, @PathVariable("reportId") long reportId) {
-        FeverReportResponseDTO.FeverReportViewDTO feverReportViewDTO = feverReportQueryService.getFeverReport(userDetails.getParent(), childId, reportId);
+        FeverReportResponseDTO.FeverReportViewDTO feverReportViewDTO = feverReportQueryService.getFeverReport(parent, childId, reportId);
         return CustomResponse.onSuccess(feverReportViewDTO);
     }
 
-    @GetMapping("/{childId}/list")
+    @GetMapping("/list/{childId}")
     @Operation(summary = "발열 리포트 조회 API", description = "최근 발열 리포트 10개를 조회합니다.")
-    public CustomResponse<List<FeverReportResponseDTO.FeverReportViewDTO>> getFeverReports(@AuthenticationPrincipal CustomUserDetails userDetails,
+    public CustomResponse<List<FeverReportResponseDTO.FeverReportViewDTO>> getFeverReportPage(@AuthParent Parent parent,
          @PathVariable("childId") long childId, @RequestParam(value="page", defaultValue="0") int page) {
-        List<FeverReportResponseDTO.FeverReportViewDTO> feverReportViewDTOList = feverReportQueryService.getFeverReports(userDetails.getParent(),childId,page);
+        List<FeverReportResponseDTO.FeverReportViewDTO> feverReportViewDTOList = feverReportQueryService.getFeverReports(parent,childId,page);
         return CustomResponse.onSuccess(feverReportViewDTOList);
     }
 
+    @GetMapping("/all/{childId}")
+    @Operation(summary = "전체 발열 리포트 조회 API", description = "전체 발열 리포트를 조회합니다.")
+    public CustomResponse<List<FeverReportResponseDTO.FeverReportViewDTO>> getFeverReportList(@AuthParent Parent parent,
+       @PathVariable("childId") long childId) {
+        List<FeverReportResponseDTO.FeverReportViewDTO> feverReportViewDTOList = feverReportQueryService.getFeverReportList(parent,childId);
+        return CustomResponse.onSuccess(feverReportViewDTOList);
+    }
+
+
     @DeleteMapping("/{childId}/{reportId}")
     @Operation(summary = "발열 리포트 삭제 API", description = "발열 리포트를 삭제합니다.")
-    public CustomResponse<Void> deleteFeverReport(@AuthenticationPrincipal CustomUserDetails userDetails,
+    public CustomResponse<Void> deleteFeverReport(@AuthParent Parent parent,
      @PathVariable("childId") long childId, @PathVariable("reportId") long reportId){
-        feverReportService.deleteFeverReport(userDetails.getParent(),reportId,childId);
+        feverReportService.deleteFeverReport(parent,reportId,childId);
         return CustomResponse.onSuccess(null);
     }
 
     @PostMapping("/{childId}")
     @Operation(summary = "발열 리포트 생성 API", description = "발열 리포트를 생성합니다.")
-    public CustomResponse<FeverReportResponseDTO.FeverReportViewDTO> createFeverReport(@AuthenticationPrincipal CustomUserDetails userDetails,
+    public CustomResponse<FeverReportResponseDTO.FeverReportViewDTO> createFeverReport(@AuthParent Parent parent,
     @RequestBody FeverReportRequestDTO.FeverReportCreateDTO feverReportRequestDTO, @PathVariable("childId") long childId) {
-        FeverReportResponseDTO.FeverReportViewDTO feverReportViewDTO = feverReportService.createFeverReport(userDetails.getParent(), feverReportRequestDTO, childId);
+        FeverReportResponseDTO.FeverReportViewDTO feverReportViewDTO = feverReportService.createFeverReport(parent, feverReportRequestDTO, childId);
         return CustomResponse.onSuccess(feverReportViewDTO);
     }
 }
