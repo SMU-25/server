e3817b9b4a62b717bd5d47d0b70ab4abefdf4e3e
JUNE <jh010303@naver.com>
2025-06-13 18:25:59 +0900

    :sparkles: feat: symptomType으로 symptom 찾는 함수 추가
    - 이미 존재하는 Symptom 추가 중복 방지

--- src/main/java/final_project/momeasy/domain/symptom/repository/SymptomRepository.java
+++ src/main/java/final_project/momeasy/domain/symptom/repository/SymptomRepository.java
@@ -1,14 +1,17 @@
 package final_project.momeasy.domain.symptom.repository;
 
-import final_project.momeasy.domain.illness.entity.Illness;
+import final_project.momeasy.common.enums.SymptomType;
 import final_project.momeasy.domain.symptom.entity.Symptom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SymptomRepository extends JpaRepository<Symptom, Long> {
     @Query("select s from Symptom s join fetch FeverReportSymptom fs on s.id = fs.symptom.id where fs.feverreport.id =:feverreportId")
     List<Symptom> findByFeverReportId(@Param("feverreportId") Long FeverReportId);
+
+    Optional<Symptom> findBySymptom(SymptomType symptomType);
 }
