9a28ea8264d44cb3bb4194da6dc3936d5ef5fee3
JUNE <jh010303@naver.com>
2025-06-29 00:33:12 +0900

    :sparkles: feat: 발열 리포트 생성, 세부 조회 DTO converter 추가
    - 그래프 정보 주입
    - getGraphGroup~ 함수는 converter에서만 사용, private으로 선언

--- src/main/java/final_project/momeasy/domain/fever_report/converter/FeverReportConverter.java
+++ src/main/java/final_project/momeasy/domain/fever_report/converter/FeverReportConverter.java
@@ -1,9 +1,18 @@
 package final_project.momeasy.domain.fever_report.converter;
 
 
+import final_project.momeasy.common.enums.DayRange;
+import final_project.momeasy.domain.fever_graph.converter.FeverGraphConverter;
+import final_project.momeasy.domain.fever_graph.entity.FeverGraph;
 import final_project.momeasy.domain.fever_report.dto.FeverReportRequestDTO;
 import final_project.momeasy.domain.fever_report.dto.FeverReportResponseDTO;
 import final_project.momeasy.domain.fever_report.entity.FeverReport;
+import final_project.momeasy.domain.humidity_graph.converter.HumidityGraphConverter;
+import final_project.momeasy.domain.humidity_graph.entity.HumidityGraph;
+import final_project.momeasy.domain.temperature_graph.converter.TemperatureGraphConverter;
+import final_project.momeasy.domain.temperature_graph.entity.TemperatureGraph;
+
+import java.util.List;
 
 
 public class FeverReportConverter {
@@ -17,11 +26,79 @@ public class FeverReportConverter {
                 .build();
     }
 
+    public static FeverReportResponseDTO.FeverReportDetailViewDTO toFeverReportDetailDTO(FeverReport feverReport,
+         List<FeverGraph> feverGraphs, List<HumidityGraph> humidityGraphs, List<TemperatureGraph> temperatureGraphs) {
+        return FeverReportResponseDTO.FeverReportDetailViewDTO.builder()
+                .outing(feverReport.getOuting())
+                .special(feverReport.getSpecial())
+                .etc_symptom(feverReport.getEtc_symptom())
+                .symptoms(feverReport.getFeverReportSymptoms().stream().map(FeverReportSymptom -> FeverReportSymptom.getSymptom().getSymptom()).toList())
+                .createdAt(feverReport.getCreatedAt())
+                .day1(getGraphGroupViewDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day1))
+                .day3(getGraphGroupViewDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day3))
+                .day7(getGraphGroupViewDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day7))
+                .build();
+    }
+
+    public static FeverReportResponseDTO.FeverReportCreateDTO toFeverReportCreateDTO(FeverReport feverReport,
+         List<FeverGraph> feverGraphs, List<HumidityGraph> humidityGraphs, List<TemperatureGraph> temperatureGraphs) {
+        return FeverReportResponseDTO.FeverReportCreateDTO.builder()
+                .outing(feverReport.getOuting())
+                .special(feverReport.getSpecial())
+                .etc_symptom(feverReport.getEtc_symptom())
+                .symptoms(feverReport.getFeverReportSymptoms().stream().map(FeverReportSymptom -> FeverReportSymptom.getSymptom().getSymptom()).toList())
+                .createdAt(feverReport.getCreatedAt())
+                .day1(getGraphGroupCreateDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day1))
+                .day3(getGraphGroupCreateDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day3))
+                .day7(getGraphGroupCreateDTO(feverGraphs,humidityGraphs,temperatureGraphs,DayRange.Day7))
+                .build();
+    }
+
     public static FeverReport toFeverReport(FeverReportRequestDTO.FeverReportCreateDTO feverReportCreateDTO, String special) {
         return FeverReport.builder()
                 .outing(feverReportCreateDTO.getOuting())
                 .special(special)
                 .etc_symptom(feverReportCreateDTO.getEtc_symptom())
+                .build();
+    }
+
+    private static FeverReportResponseDTO.FeverReportDetailViewDTO.GraphGroupDTO getGraphGroupViewDTO(List<FeverGraph> feverGraphs,
+                                                                                                      List<HumidityGraph> humidityGraphs,
+                                                                                                      List<TemperatureGraph> temperatureGraphs,
+                                                                                                  DayRange dayRange) {
+        return FeverReportResponseDTO.FeverReportDetailViewDTO.GraphGroupDTO.builder()
+                .fever(feverGraphs.stream()
+                        .filter(graph -> graph.getDayRange() == dayRange)
+                        .map(FeverGraphConverter::toFeverGraphViewDTO)
+                        .toList())
+                .temperature(temperatureGraphs.stream()
+                       .filter(graph -> graph.getDayRange() == dayRange)
+                       .map(TemperatureGraphConverter::toTemperatureGraphViewDTO)
+                        .toList())
+                .humidity(humidityGraphs.stream()
+                        .filter(graph -> graph.getDayRange() == dayRange)
+                        .map(HumidityGraphConverter::toHumidityGraphViewDTO)
+                        .toList())
+                .build();
+    }
+
+    private static FeverReportResponseDTO.FeverReportCreateDTO.GraphGroupDTO getGraphGroupCreateDTO(List<FeverGraph> feverGraphs,
+                                                                                                    List<HumidityGraph> humidityGraphs,
+                                                                                                    List<TemperatureGraph> temperatureGraphs,
+                                                                                                  DayRange dayRange) {
+        return FeverReportResponseDTO.FeverReportCreateDTO.GraphGroupDTO.builder()
+                .fever(feverGraphs.stream()
+                        .filter(graph -> graph.getDayRange() == dayRange)
+                        .map(FeverGraphConverter::toFeverGraphCreateDTO)
+                        .toList())
+                .temperature(temperatureGraphs.stream()
+                        .filter(graph -> graph.getDayRange() == dayRange)
+                        .map(TemperatureGraphConverter::toTemperatureGraphCreateDTO)
+                        .toList())
+                .humidity(humidityGraphs.stream()
+                        .filter(graph -> graph.getDayRange() == dayRange)
+                       .map(HumidityGraphConverter::toHumidityGraphCreateDTO)
+                        .toList())
                 .build();
     }
 }
