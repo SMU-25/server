ec4109dff162ab36ed12736c5c71dc994f16b8b7
JUNE <jh010303@naver.com>
2025-06-29 00:31:03 +0900

    :sparkles: feat: 발열 리포트 생성, 세부 조회 DTO 추가
    - response에 그래프 정보 추가

--- src/main/java/final_project/momeasy/domain/fever_report/dto/FeverReportResponseDTO.java
+++ src/main/java/final_project/momeasy/domain/fever_report/dto/FeverReportResponseDTO.java
@@ -1,6 +1,9 @@
 package final_project.momeasy.domain.fever_report.dto;
 
 import final_project.momeasy.common.enums.SymptomType;
+import final_project.momeasy.domain.fever_graph.dto.FeverGraphResponseDTO;
+import final_project.momeasy.domain.humidity_graph.dto.HumidityGraphResponseDTO;
+import final_project.momeasy.domain.temperature_graph.dto.TemperatureGraphResponseDTO;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,5 +23,51 @@ public class FeverReportResponseDTO {
         private String special;
 
         private LocalDateTime createdAt;
+    }
+
+    @Getter
+    @Builder
+    public static class FeverReportDetailViewDTO {
+
+        private List<SymptomType> symptoms;
+        private String etc_symptom;
+        private String outing;
+        private String special;
+        private LocalDateTime createdAt;
+
+        private GraphGroupDTO day1;
+        private GraphGroupDTO day3;
+        private GraphGroupDTO day7;
+
+        @Getter
+        @Builder
+        public static class GraphGroupDTO {
+            private List<FeverGraphResponseDTO.FeverGraphViewDTO> fever;
+            private List<HumidityGraphResponseDTO.HumidityGraphViewDTO> humidity;
+            private List<TemperatureGraphResponseDTO.TemperatureGraphViewDTO> temperature;
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class FeverReportCreateDTO {
+
+        private List<SymptomType> symptoms;
+        private String etc_symptom;
+        private String outing;
+        private String special;
+        private LocalDateTime createdAt;
+
+        private GraphGroupDTO day1;
+        private GraphGroupDTO day3;
+        private GraphGroupDTO day7;
+
+        @Getter
+        @Builder
+        public static class GraphGroupDTO {
+            private List<FeverGraphResponseDTO.FeverGraphCreateDTO> fever;
+            private List<HumidityGraphResponseDTO.HumidityGraphCreateDTO> humidity;
+            private List<TemperatureGraphResponseDTO.TemperatureGraphCreateDTO> temperature;
+        }
     }
 }
