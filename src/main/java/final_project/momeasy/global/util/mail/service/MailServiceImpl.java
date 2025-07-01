9b60c56cf2c26356c134d3506d0eecffbf16df96
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-07-01 19:49:22 +0900

    🔨 Refactor: 불필요한 애노테이션 삭제

--- src/main/java/final_project/momeasy/global/util/mail/service/MailServiceImpl.java
+++ src/main/java/final_project/momeasy/global/util/mail/service/MailServiceImpl.java
@@ -6,12 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class MailServiceImpl implements MailService {
 
     private final JavaMailSender mailSender;
