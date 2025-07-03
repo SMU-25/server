3cef1ff33e4e805b76dbc4c56cb601b0f1a23e93
JUNE <jh010303@naver.com>
2025-07-03 17:45:01 +0900

    :hammer: refactor: 홈캠 목록 조회 dto에 serialNum 추가

--- src/main/java/final_project/momeasy/domain/home_cam/dto/HomecamResponseDTO.java
+++ src/main/java/final_project/momeasy/domain/home_cam/dto/HomecamResponseDTO.java
@@ -2,21 +2,24 @@ package final_project.momeasy.domain.home_cam.dto;
 
 import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class HomecamResponseDTO {
     @Getter
     @Builder
     public static class HomecamDTO {
+        private Long homecamId;
         private String name;
         private String place;
         private LocalDateTime createdAt;
+        private String serialNum;
     }
 
     @Getter
     @Builder
-    public static class HomecamVideoDTO {
+    public static class HomecamDetailDTO {
+        private String name;
+        private String place;
         private String video_url;
     }
 }
