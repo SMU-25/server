458eed8f28c0181ed7a6f0fa6c78f23c38f24c81
Jeonghyeon Shin <tlswjdgus1016@naver.com>
2025-07-03 16:02:08 +0900

    🔨 Refactor: CalendarResponseDto 변환 로직을 Converter로 분리

--- src/main/java/final_project/momeasy/domain/calendar/dto/CalendarResponseDto.java
+++ src/main/java/final_project/momeasy/domain/calendar/dto/CalendarResponseDto.java
@@ -1,6 +1,5 @@
 package final_project.momeasy.domain.calendar.dto;
 
-import final_project.momeasy.domain.calendar.entity.Calendar;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,17 +8,8 @@ import java.time.LocalDate;
 @Getter
 @Builder
 public class CalendarResponseDto {
-    private Long id;
+    private Long calendarId;
     private LocalDate scheduleDate;
     private String title;
     private String content;
-
-    public static CalendarResponseDto fromEntity(Calendar calendar) {
-        return CalendarResponseDto.builder()
-                .id(calendar.getId())
-                .scheduleDate(calendar.getScheduleDate())
-                .title(calendar.getTitle())
-                .content(calendar.getContent())
-                .build();
-    }
 }
