70b3f132c94ca1693f3d6dcb89d869b00cb68548
Hyeonbeen Park <hyunbini2942@gmail.com>
2025-07-07 23:39:44 +0900

    🔨 Refactor: 커스텀 애노테이션 객체 수정

--- src/main/java/final_project/momeasy/domain/parent/controller/ParentController.java
+++ src/main/java/final_project/momeasy/domain/parent/controller/ParentController.java
@@ -1,6 +1,7 @@
 package final_project.momeasy.domain.parent.controller;
 
 import final_project.momeasy.domain.parent.dto.response.ParentResponseDTO;
+import final_project.momeasy.domain.parent.entity.Parent;
 import final_project.momeasy.domain.parent.service.command.ParentCommandService;
 import final_project.momeasy.domain.parent.service.query.ParentQueryService;
 import final_project.momeasy.global.apiPayload.CustomResponse;
@@ -19,16 +20,16 @@ public class ParentController {
     private final ParentQueryService parentQueryService;
 
     @DeleteMapping
-    public CustomResponse<String> deleteParent(@AuthParent CustomUserDetails customUserDetails) {
-        Long parentId = customUserDetails.getParent().getId();
+    public CustomResponse<String> deleteParent(@AuthParent Parent parent) {
+        Long parentId = parent.getId();
         parentCommandService.deleteParent(parentId);
         return CustomResponse.onSuccess(HttpStatus.NO_CONTENT, "회원 탈퇴 완료");
     }
 
     @GetMapping
     public CustomResponse<ParentResponseDTO.ParentDetailResponseDTO> getParent(
-            @AuthParent CustomUserDetails customUserDetails) {
-        Long parentId = customUserDetails.getParent().getId();
+            @AuthParent Parent parent) {
+        Long parentId = parent.getId();
         return CustomResponse.onSuccess(parentQueryService.getParentDetail(parentId));
     }
 
