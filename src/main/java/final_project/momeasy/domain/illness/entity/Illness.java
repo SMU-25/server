071ede0b3390f65ce0c3d36c94e747c6dcdc484a
Shin406 <tlswjdgus1016@naver.com>
2025-07-08 13:53:46 +0900

    ✨ feat: Illness 도메인 코드 리팩토링

--- src/main/java/final_project/momeasy/domain/illness/entity/Illness.java
+++ src/main/java/final_project/momeasy/domain/illness/entity/Illness.java
@@ -1,13 +1,18 @@
 package final_project.momeasy.domain.illness.entity;
 
 import final_project.momeasy.common.enums.IllnessType;
+import final_project.momeasy.domain.child.entity.Child;
+import final_project.momeasy.domain.child.entity.ChildIllness;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class Illness {
 
@@ -17,4 +22,9 @@ public class Illness {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
     private IllnessType illnessType;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "illness", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<ChildIllness> childIllnesses = new ArrayList<>();
+
 }
