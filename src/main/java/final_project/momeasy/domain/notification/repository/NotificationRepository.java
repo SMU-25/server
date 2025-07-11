6413c9dfbd97241ea65acbd9b983abdfabee10de
Shin406 <tlswjdgus1016@naver.com>
2025-07-11 14:37:52 +0900

    ✨ feat: NotificationRepository 생성

--- src/main/java/final_project/momeasy/domain/notification/repository/NotificationRepository.java
+++ src/main/java/final_project/momeasy/domain/notification/repository/NotificationRepository.java
@@ -4,12 +4,10 @@ import final_project.momeasy.domain.notification.entity.Notification;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Slice<Notification> findByParentId(long parentid, Pageable pageable);
-    Optional<Notification> findTopByParentIdAndChildIdOrderByIdDesc(long parentid, long childid);
+    Slice<Notification> findByParentId(long parentId, Pageable pageable);
+    Optional<Notification> findTopByParentIdAndChildIdOrderByIdDesc(long parentId, long childId);
 }
