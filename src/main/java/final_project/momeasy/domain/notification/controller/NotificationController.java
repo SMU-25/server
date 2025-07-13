33c098f211502be2f8671d9c34fd355e3a45bf68
Shin406 <tlswjdgus1016@naver.com>
2025-07-13 18:54:16 +0900

    🔨 refactor: 기존 Pageable 기반 조회 API 제거 및 커서 방식 적용

--- src/main/java/final_project/momeasy/domain/notification/controller/NotificationController.java
+++ src/main/java/final_project/momeasy/domain/notification/controller/NotificationController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
