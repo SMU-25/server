054d08e28dc40e1362f82c1d671a8a08bcd549ff
Shin406 <tlswjdgus1016@naver.com>
2025-08-29 01:25:51 +0900

    ✨ feat: FCM 토큰 저장/삭제 API 추가

---
+++ src/main/java/final_project/momeasy/global/fcm/controller/FcmTokenController.java
@@ -0,0 +1,37 @@
+package final_project.momeasy.global.fcm.controller;
+
+import final_project.momeasy.global.apiPayload.CustomResponse;
+import final_project.momeasy.global.fcm.dto.FcmTokenRequest;
+import final_project.momeasy.global.fcm.service.FcmTokenService;
+import final_project.momeasy.global.security.CustomUserDetails;
+import final_project.momeasy.global.security.annotation.AuthParent;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/fcm-token")
+@RequiredArgsConstructor
+public class FcmTokenController {
+
+    private final FcmTokenService tokenService;
+
+    @PostMapping
+    public CustomResponse<String> saveToken(
+            @AuthParent CustomUserDetails customUserDetails,
+            @Valid @RequestBody FcmTokenRequest request
+    ) {
+        tokenService.saveToken(customUserDetails.getParent(), request.token(), request.deviceType());
+        return CustomResponse.onSuccess(HttpStatus.CREATED, "FCM 토큰 저장 완료");
+    }
+
+    @DeleteMapping
+    public CustomResponse<String> deleteToken(
+            @AuthParent CustomUserDetails customUserDetails,
+            @RequestParam String token
+    ) {
+        tokenService.deleteToken(customUserDetails.getParent(), token);
+        return CustomResponse.onSuccess(HttpStatus.OK, "FCM 토큰 삭제 완료");
+    }
+}
