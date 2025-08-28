dba3e906d8230b58f4df87ff4cbe81b8a82de909
Shin406 <tlswjdgus1016@naver.com>
2025-08-29 01:26:49 +0900

    ✨ feat: FCM 토큰 요청 DTO 추가

---
+++ src/main/java/final_project/momeasy/global/fcm/dto/FcmTokenRequest.java
@@ -0,0 +1,10 @@
+package final_project.momeasy.global.fcm.dto;
+
+import final_project.momeasy.common.enums.DeviceType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record FcmTokenRequest(
+        @NotBlank String token,
+        @NotNull DeviceType deviceType
+) {}
