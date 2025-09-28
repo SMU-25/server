936f133c1303ef34e9c73e3324fd88543b1d1399
JUNE <jh010303@naver.com>
2025-09-29 01:21:44 +0900

    :sparkles: feat: 지도 즐겨찾기 Service
    - 사용자의 즐겨찾기 조회, 페이지네이션 목록 조회
    - 지도 즐겨찾기 생성, 삭제

--- src/main/java/final_project/momeasy/domain/map_favorites/service/MapFavoritesCommandServiceImpl.java
+++ src/main/java/final_project/momeasy/domain/map_favorites/service/MapFavoritesCommandServiceImpl.java
@@ -9,30 +9,36 @@ import final_project.momeasy.domain.map_favorites.exception.MapFavortiesErrorCod
 import final_project.momeasy.domain.map_favorites.repository.MapFavoritesRepository;
 import final_project.momeasy.domain.parent.entity.Parent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MapFavoritesCommandServiceImpl implements MapFavoritesCommandService {
 
     private final MapFavoritesRepository mapFavoritesRepository;
 
     public MapFavoritesResponseDTO.MapFavoritesCreateDTO createMapFavorites(Parent parent, MapFavoritesRequestDTO.MapFavoritesCreateDTO mapFavoritesCreateDTO) {
         if(parent == null){
+            log.warn("부모 정보 없음");
             throw new MapFavoritesException(MapFavortiesErrorCode.UNAUTHORIZED_ACCESS);
         }
         MapFavorites mapFavorites = MapFavoritesConverter.toMapFavorites(mapFavoritesCreateDTO);
         mapFavorites.setParent(parent);
         mapFavoritesRepository.save(mapFavorites);
+        log.info("지도 즐겨찾기 생성 완료");
         return MapFavoritesConverter.toMapFavoritesCreateDTO(mapFavorites);
     }
 
     public void deleteMapFavorites(Parent parent, Long mapId) {
         Long parentId = parent.getId();
         if(!mapFavoritesRepository.existsByMapIdAndParentId(mapId, parentId)){
+            log.warn("지도 즐겨찾기 삭제 권한 없음");
             throw new MapFavoritesException(MapFavortiesErrorCode.UNAUTHORIZED_ACCESS);
         }
         MapFavorites mapFavorites = mapFavoritesRepository.findById(mapId).orElseThrow(() -> new MapFavoritesException(MapFavortiesErrorCode.NOT_FOUND));
+        log.info("지도 즐겨찾기 삭제 완료");
         mapFavoritesRepository.delete(mapFavorites);
     }
 }
--- src/main/java/final_project/momeasy/domain/map_favorites/service/MapFavoritesQueryServiceImpl.java
+++ src/main/java/final_project/momeasy/domain/map_favorites/service/MapFavoritesQueryServiceImpl.java
@@ -8,15 +8,19 @@ import final_project.momeasy.domain.map_favorites.exception.MapFavortiesErrorCod
 import final_project.momeasy.domain.map_favorites.repository.MapFavoritesRepository;
 import final_project.momeasy.domain.parent.entity.Parent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class MapFavoritesQueryServiceImpl implements MapFavoritesQueryService {
 
     private final MapFavoritesRepository mapFavoritesRepository;
@@ -26,13 +30,16 @@ public class MapFavoritesQueryServiceImpl implements MapFavoritesQueryService {
         MapFavorites mapFavorites = mapFavoritesRepository.findById(mapId).orElseThrow(
                 ()->new MapFavoritesException(MapFavortiesErrorCode.NOT_FOUND));
         if(!mapFavoritesRepository.existsByMapIdAndParentId(mapId, parent.getId())) {
+            log.warn("지도 즐겨찾기 조회 권한 없음");
             throw new MapFavoritesException(MapFavortiesErrorCode.UNAUTHORIZED_ACCESS);
         }
+        log.info("지도 즐겨찾기 조회 성공");
         return MapFavoritesConverter.toMapFavoritesDetailDTO(mapFavorites);
     }
 
     public MapFavoritesResponseDTO.MapFavoritesListViewDTO getMapFavoritesList(Parent parent, Long cursor, Integer size) {
         if(parent==null) {
+            log.warn("지도 즐겨찾기 조회 권한 없음");
             throw new MapFavoritesException(MapFavortiesErrorCode.UNAUTHORIZED_ACCESS);
         }
         Long parentId = parent.getId();
@@ -46,6 +53,7 @@ public class MapFavoritesQueryServiceImpl implements MapFavoritesQueryService {
                 mapFavoritesList.stream().map(MapFavoritesConverter::toMapFavoritesViewDTO).toList();
 
         if(mapFavoritesSlice.isEmpty()){
+            log.warn("지도 즐겨찾기 데이터 없음");
             throw new MapFavoritesException(MapFavortiesErrorCode.NOT_FOUND);
         }
 
@@ -53,6 +61,7 @@ public class MapFavoritesQueryServiceImpl implements MapFavoritesQueryService {
         if(!mapFavoritesList.isEmpty() && mapFavoritesSlice.hasNext()){
             nextCursor = mapFavoritesList.get(mapFavoritesList.size()-1).getId();
         }
+        log.info("지도 즐겨찾기 목록 조회 성공");
         return MapFavoritesConverter.toMapFavoritesListViewDTO(mapFavoritesListViewDTO,nextCursor,mapFavoritesSlice.hasNext());
 
     }
