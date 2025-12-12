4a2d9ad283d209bacbdc04c091f368caed59dbe9
JUNE <jh010303@naver.com>
2025-12-13 00:22:04 +0900

    :hammer: 코드 중복 제거
    - 평균값 구하는 함수 생성

--- src/main/java/final_project/momeasy/domain/temperature_graph/service/AvgTemperature.java
+++ src/main/java/final_project/momeasy/domain/temperature_graph/service/AvgTemperature.java
@@ -15,26 +15,25 @@ public class AvgTemperature {
     private final RoomConditionRepository roomConditionRepository;
 
     public float getTemperatureAvgBy3Hour(int time, Long childId) {
-        LocalDateTime start = LocalDate.now().atTime(time,0);
+        LocalDateTime start = LocalDate.now().atTime(time, 0);
         LocalDateTime end = (time == 21)
-                ?LocalDate.now().plusDays(1).atStartOfDay()
-                :LocalDate.now().atTime(time+3,0);
-        List<RoomCondition> roomConditions = roomConditionRepository.findByChildIdAndCreatedAtBetween(childId,start,end);
-        return (float) Math.round(
-                roomConditions.stream()
-                        .mapToDouble(RoomCondition::getTemperature)
-                        .average()
-                        .orElse(0.0) * 10
-        ) / 10;
+                ? LocalDate.now().plusDays(1).atStartOfDay()
+                : LocalDate.now().atTime(time + 3, 0);
+        List<RoomCondition> roomConditions = roomConditionRepository.findByChildIdAndCreatedAtBetween(childId, start, end);
+        return avgTemperature(roomConditions);
     }
 
     public float getTemperatureAvgByDayAndTimeRange(int day, int time1, int time2, Long childId) {
         LocalDate now = LocalDate.now().minusDays(day);
-        LocalDateTime start = now.atTime(time1,0);
+        LocalDateTime start = now.atTime(time1, 0);
         LocalDateTime end = (time2 == 24)
                 ? now.plusDays(1).atStartOfDay()
                 : now.atTime(time2, 0);
-        List<RoomCondition> roomConditions = roomConditionRepository.findByChildIdAndCreatedAtBetween(childId, start,end);
+        List<RoomCondition> roomConditions = roomConditionRepository.findByChildIdAndCreatedAtBetween(childId, start, end);
+        return avgTemperature(roomConditions);
+    }
+
+    private float avgTemperature(List<RoomCondition> roomConditions) {
         return (float) Math.round(
                 roomConditions.stream()
                         .mapToDouble(RoomCondition::getTemperature)
